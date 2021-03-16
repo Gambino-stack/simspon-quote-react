c467065d966ce5181fca66fad80a95cd72bbbb38
yohan <yohan.lgd@gmail.com>
2021-03-16 11:45:04 +0100

    quete react 8

--- React WCS Quetes/simpsons-quotes/simpsons-quotes2/src/App.js
+++ React WCS Quetes/simpsons-quotes/simpsons-quotes2/src/App.js
@@ -4,32 +4,28 @@ import QuoteCard from './components/QuoteCard';
 import axios from 'axios';
 import LoadingSpinner from './components/LoadingSpinner';
 
-const defaultQuote =  {
-  image : "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FMargeSimpson.png?1497567512205",
-  quote : "I don't want to sound like a killjoy, but becuase this is not to my taste I don't think anyone else should be allowed to enjoy it.",
-  character : "Marge Simspsons",
-};
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote : defaultQuote,
+      quote : null,
       loading: false,
     };
     this.getQuote = this.getQuote.bind(this);
   }
 
+  componentDidMount() {
+    this.getQuote();
+  }
+
   getQuote () {
-    this.setState({ loading: true }, () => {
       axios.get('https://simpsons-quotes-api.herokuapp.com/quotes')
         .then(result => result.data)
         .then(data => {
         this.setState({
-          loading: false,
           quote: data[0],
-        });
-      });   
+        });   
     })
   }
     
@@ -37,8 +33,8 @@ class App extends React.Component {
     const {loading, quote} = this.state;
     return (
       <div className="App">
+         {quote ? <QuoteCard quote={this.state.quote} /> : <LoadingSpinner /> }
         <button type="button" onClick={this.getQuote}>Get Quote </button>
-        {loading ? <LoadingSpinner /> : <QuoteCard quote={this.state.quote} />}
         
       </div>
     );
